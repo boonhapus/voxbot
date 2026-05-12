9e29703ed3d67ba7c4444a4f7f24973238b9dc55
boonhapus <me@boonhap.us>
2026-05-12 11:04:29 -0500

    Wait for current symlink before cd

--- deploy/macos/apps/run-bot.sh
+++ deploy/macos/apps/run-bot.sh
@@ -6,6 +6,12 @@ set -a
 source /Users/voxbot/secrets/voxbot.env
 set +a
 
+# Wait for the current symlink to exist
+until [ -L /Users/voxbot/apps/voxbot/current ]; do
+  echo "Waiting for /Users/voxbot/apps/voxbot/current to be available..."
+  sleep 5
+done
+
 cd /Users/voxbot/apps/voxbot/current
 export VOXBOT_RELEASE_SHA="$(git rev-parse HEAD)"
 
